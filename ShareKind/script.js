4fb75f73604830e256d4f29039d90a2174027fb3
MuthuRahul <rahulpitchiah2004@gmail.com>
2024-09-11 00:42:16 -0700

    Add files via upload

---
+++ ShareKind/script.js
@@ -0,0 +1,25 @@
+var TrendingSlider = new Swiper('.trending-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: '3', // Adjusts the number of slides visible based on container size
+    spaceBetween: 20,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    lazy: {
+        loadPrevNext: true,
+    },
+});
